bd1f144de602d6ff881d680988239df83b6eaf6e
lcostantini <costantinileandro1@gmail.com>
2016-03-08 15:46:30 -0300

    Add anomaly button.

--- app.rb
+++ app.rb
@@ -1,15 +1,29 @@
 require 'cuba'
 
+$requests = 10
+
 Cuba.define do
   on root do
-    res.write 'hello'
+    res.write "<form action='/health-check'><input type='submit' name='anomaly' value='Add Anomaly'></form>"
   end
 
   on 'health-check' do
-    if rand > 0.8
-      res.status = 500
+    if env["QUERY_STRING"].empty? && $requests == 10
+      $requests = 10
+      if rand > 0.8
+        res.status = 500
+      else
+        res.status = 200
+      end
     else
-      res.status = 200
+      $requests -= 1
+      $requests = 10 if $requests == 0
+      if rand > 0.7
+        sleep(0.9)
+        res.status = 200
+      else
+        res.status = 500
+      end
     end
   end
 end
